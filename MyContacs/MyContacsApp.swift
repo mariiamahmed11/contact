09e2e0837d81e122a6d39088ef4324696b6f06c3
mariam <mariiamahmed11@hotmail.com>
2023-06-07 05:06:59 +0300

    Initial Commit

---
+++ MyContacs/MyContacsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyContacsApp.swift
+//  MyContacs
+//
+//  Created by user on 07/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyContacsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
